f899cbb2d381daf352a473b974b35209e61bb271
Chaitanya Gautam <chaitanyag731@gmail.com>
2023-03-09 11:16:45 +0530

    function program

---
+++ function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+cout<<"hel"<<endl;
+cout<<"hello"<<" how are you"<<endl;
+cout<<"hello"<<" how are you"<<endl;
+return 0;
+cout<<"what is your age"<<endl;
+}
