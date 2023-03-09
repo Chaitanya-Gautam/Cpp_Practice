79e3d6dd3b934850012a9c32b16bec39372be613
Chaitanya Gautam <chaitanyag731@gmail.com>
2023-03-09 10:58:51 +0530

    age program added.

---
+++ age_prgm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+// AGE prgm 
+int main(){
+int age;
+cout<<"Enter your age"<<endl;
+cin>>age;
+cout<<"your age is:"<<" "<<age;
+
+}
+
+// USING getline() FUNCTION ;
+// int main(){
+//     char str[100];
+//     cout<<"Enter your name: ";
+//     cin.getline(str,40,'\n');
+//     cout<<"The name you provided is:"<<str<<endl;
+//     return 0;
+// }
